ebe68602cb5568c6bc6c121811cbcc72186c80b3
Rand Hale <randluca@gmail.com>
2021-01-26 19:07:03 -0800

    add GET route for /api/notes and return contents of db.json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var db = require("./db/db.json");
 var app = express();
 
 var PORT = process.env.PORT || 3003;
@@ -15,13 +16,14 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*", function(req, res) {
+// Can move this GET method to the bottom of the GETs and replace '/' with '*' to be a catch-all, but gotta make sure it's the bottom one so it doesn't get caught first.
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-
-
-
+app.get("/api/notes", function(req, res) {
+    res.json(db);
+})
 
 // Start the server
 app.listen(PORT, function() {
